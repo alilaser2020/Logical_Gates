ddcdc9120d2c368f89c43aa8fdd8b5d2d3dee9dc
alilaser <alilaser8080@gmail.com>
2024-01-03 15:29:46 +0330

    Add version4: or_gate

--- main.py
+++ main.py
@@ -1,23 +1,30 @@
 import PySimpleGUI as sg
 
 
-def logical_gates(and1, and2):
+def logical_gates(and1, and2, or1, or2):
     """
     A method for process of input gates and generate appropriate output of gates
     :param and1:
     :param and2:
+    :param or1:
+    :param or2:
     :return:
     """
     if bool(and1) and bool(and2):
-        window["-OFF-"].update("gates/on.png")
+        window["-OFF1-"].update("gates/on.png")
     else:
-        window["-OFF-"].update("gates/off.png")
+        window["-OFF1-"].update("gates/off.png")
+
+    if bool(or1) or bool(or2):
+        window["-OFF2-"].update("gates/on.png")
+    else:
+        window["-OFF2-"].update("gates/off.png")
 
 
-sg.theme("LightGray6")
+sg.theme("LightBlue7")
 
 first_layout = [
-    [sg.Text("Welcome to Gates Tutorial Application (GTA)", font="Calibre 30", text_color="red", expand_x=True,
+    [sg.Text("Welcome to\nGates Tutorial Application (GTA)", font="Calibre 30", text_color="red", expand_x=True,
              justification="center")],
     [sg.Text("Click tabs to start", font="Caliber 25", text_color="blue", expand_x=True, justification="center")],
     [sg.Image("gates/start.png", expand_x=True)],
@@ -29,13 +36,19 @@ and_btn = sg.Column([
     [sg.Slider(range=(0, 1), key="-ANDBTN2-", size=(4, 30), pad=(0, 5))]
 ])
 
+# off_image = sg.Column([
+#     [
+#         sg.Image("gates/off.png", key="-OFF-", expand_x=True, expand_y=True, pad=0)
+#     ]
+# ])
+
 and_layout = [
     [
         sg.Column([
             [
                 and_btn,
                 sg.Image("gates/and.png", expand_x=True, expand_y=True, key="-ANDGATE-", pad=0),
-                sg.Image("gates/off.png", expand_x=True, expand_y=True, key="-OFF-", pad=0)
+                sg.Image("gates/off.png", key="-OFF1-", expand_x=True, expand_y=True, pad=0)
             ]
         ], justification="center")
     ],
@@ -48,11 +61,32 @@ and_layout = [
     ]
 ]
 
+or_btn = sg.Column([
+    [sg.Slider(range=(0, 1), key="-ORBTN1-", size=(4, 30), pad=(0, 5))],
+    [sg.Slider(range=(0, 1), key="-ORBTN2-", size=(4, 30), pad=(0, 5))]
+])
+
+or_layout = [
+    [sg.Column([
+        [
+            or_btn,
+            sg.Image("gates/or.png", key="-OR-", expand_x=True, expand_y=True, pad=0),
+            sg.Image("gates/off.png", key="-OFF2-", expand_x=True, expand_y=True, pad=0)
+        ]
+    ], justification="center")],
+    [sg.Column([
+        [
+            sg.Image("gates/or_table.png", expand_x=True, expand_y=True)
+        ]
+    ], justification="center", pad=(100, 0))]
+]
+
 tab_group = [[
     sg.TabGroup([
         [
             sg.Tab("start", first_layout),
-            sg.Tab("and", and_layout)
+            sg.Tab("and", and_layout),
+            sg.Tab("or", or_layout)
         ]
     ], font="Calibre 15", tab_location="top", tab_background_color="black", title_color="white",
         selected_background_color="yellow", selected_title_color="red")
@@ -65,6 +99,9 @@ while True:
     if event == sg.WIN_CLOSED:
         break
 
-    logical_gates(values["-ANDBTN1-"], values["-ANDBTN2-"])
+    logical_gates(values["-ANDBTN1-"],
+                  values["-ANDBTN2-"],
+                  values["-ORBTN1-"],
+                  values["-ORBTN2-"])
 
 window.close()
